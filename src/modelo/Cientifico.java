90203f93aa34a891f971edfe985801ada06b62be
Byron <byronbl.12@gmail.com>
2019-06-01 14:23:34 +0100

    Solución ejercicio 4

--- src/modelo/Cientifico.java
+++ src/modelo/Cientifico.java
@@ -1,5 +1,13 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import control.LecturaEscritura;
+import modelo.dao.AccesoDatos;
+
 public class Cientifico {
 	private String dni;
 	private String nombre;
@@ -27,5 +35,38 @@ public class Cientifico {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	/**
+	 * <h2>Inserta un fichero txt en la tabla cientificos</h2>
+	 * @param conexion La conexión con la base de datos
+	 * @param ruta La ruta del fichero a insertar
+	 * @param separador Una cadena que representa el separador entre los datos de un mismo registro en el fichero
+	 * */
+	public static void insertFileIntoTable(String ruta, String separador) {
+		ArrayList<String[]> lista = LecturaEscritura.getRegistersFromFile(ruta, separador);
+
+		lista.forEach((registro) ->	insertRegister(registro));
+		
+		System.out.println("Fin de inserción de datos.");
+	}
+	
+	/**
+	 * <h2>Inserta un registro de tipo {@code String[]} en la tabla cientificos</h2>
+	 * @param registro Los datos de un cientifico en un {@code String[]}
+	 * @param conexion La conexion con la base de datos
+	 * */
+	private static void insertRegister(String[] registro) {
+		Connection conexion = AccesoDatos.getConnection();
+		String sql = "INSERT INTO cientificos (dni, nombre) VALUES (?,?)";
+		
+		try {
+			PreparedStatement pstmt = conexion.prepareStatement(sql);
+			pstmt.setString(1, registro[0]);
+			pstmt.setString(2, registro[1]);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("SQLException al insertar datos.");
+		}
 	}
 }
--- src/modelo/Proyecto.java
+++ src/modelo/Proyecto.java
@@ -1,5 +1,13 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import control.LecturaEscritura;
+import modelo.dao.AccesoDatos;
+
 public class Proyecto {
 	private String id;
 	private String nombre;
@@ -33,5 +41,39 @@ public class Proyecto {
 	}
 	public void setHoras(int horas) {
 		this.horas = horas;
+	}
+
+	/**
+	 * <h2>Inserta un fichero txt en la tabla proyectos</h2>
+	 * @param conexion La conexión con la base de datos
+	 * @param ruta La ruta del fichero a insertar
+	 * @param separador Una cadena que representa el separador entre los datos de un mismo registro en el fichero
+	 * */
+	public static void insertFileIntoTable(String ruta, String separador) {
+		ArrayList<String[]> lista = LecturaEscritura.getRegistersFromFile(ruta, separador);
+
+		lista.forEach((registro) ->	insertRegister(registro));
+		
+		System.out.println("Fin de inserción de datos.");
+	}
+	
+	/**
+	 * <h2>Inserta un registro de tipo {@code String[]} en la tabla Proyectos</h2>
+	 * @param registro Los datos de un cientifico en un {@code String[]}
+	 * @param conexion La conexion con la base de datos
+	 * */
+	private static void insertRegister(String[] registro) {
+		Connection conexion = AccesoDatos.getConnection();
+		String sql = "INSERT INTO proyectos (id, nombre, horas) VALUES (?,?,?)";
+		
+		try {
+			PreparedStatement pstmt = conexion.prepareStatement(sql);
+			pstmt.setString(1, registro[0]);
+			pstmt.setString(2, registro[1]);
+			pstmt.setInt(3, Integer.parseInt(registro[2]));
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("SQLException al insertar datos.");
+		}
 	}
 }
